5b3adfdc01ea5b8bd7d2c1849e92c91573222ee5
ShawnZK <carlyle.chou@gmail.com>
2019-05-02 11:39:08 +0800

    modify author

--- trpc-common/src/main/java/com/sigmoid/arch/trpc/common/exception/TRpcTimeoutException.java
+++ trpc-common/src/main/java/com/sigmoid/arch/trpc/common/exception/TRpcTimeoutException.java
@@ -3,7 +3,7 @@ package com.sigmoid.arch.trpc.common.exception;
 import java.util.concurrent.TimeoutException;
 
 /**
- * Created by shuyang on 2019/4/15.
+ * Created by ShawnZk on 2019/4/15.
  */
 public class TRpcTimeoutException extends TimeoutException {
 
--- trpc-rpc/src/main/java/com/sigmoid/arch/trpc/rpc/registry/ServerRegistry.java
+++ trpc-rpc/src/main/java/com/sigmoid/arch/trpc/rpc/registry/ServerRegistry.java
@@ -1,7 +1,7 @@
 package com.sigmoid.arch.trpc.rpc.registry;
 
 /**
- * Created by shuyang on 2019/4/20.
+ * Created by ShawnZk on 2019/4/20.
  */
 public class ServerRegistry {
 }
